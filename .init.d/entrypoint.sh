2fe042787a29d8bc49c12dbe6bc3f5d12d8e15a0
Zevon T Flynn <ztflynn@ztangerine.io>
2023-12-07 08:19:38 -0700

    Added essential files for build

---
+++ .init.d/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## /home/flynn/.init.d/entrypoint.sh
+## 
+## This script is run at the start of the container.
+echo
+echo "Greetings, Hologram!"
+echo
+
+## Do all that needs to be done at the start of the container.
+
+## Sleep forever.
+sleep infinity
+# EOF
